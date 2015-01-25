ab55f5abba1d70e62fcb87d1dbd122cab6e1997f
Justin <jaschultz25@gmail.com>
2015-01-24 21:07:09 -0500

    rids of bad local-game

--- js/menu/menu-top.js
+++ js/menu/menu-top.js
@@ -8,28 +8,9 @@
 
 	var TopMenu = LW.Menus.TopMenu = new LW.MainMenu({
     title: "Lil' Wizards",
-    commands: ["local-game", "character-select", "settings"],
+    commands: ["local-game", "settings"],
     events: {
       "local-game": function () {
-        LW.GlobalSL.playSE('menu-select.ogg', 100)
-
-        $('.main-menu').addClass("hidden");
-
-        var bgm = document.getElementById("bgm");
-        bgm.src = "audio/BGM/battle.mp3";
-        bgm.volume = 0.2;
-        var sound_library = new LW.SoundLibrary();
-        var fgcanvas = document.getElementById("game-fg-canvas");
-        var fgctx = fgcanvas.getContext('2d');
-        var bgcanvas = document.getElementById("game-bg-canvas");
-        var bgctx = bgcanvas.getContext('2d');
-        var gameView = new LW.GameView(bgctx, fgctx);
-        gameView.game.audio = sound_library;
-        gameView.startGame();
-        this.remove();
-        // bgm.play();
-      },
-      "character-select": function () {
         LW.GlobalSL.playSE('menu-select.ogg', 100)
         LW.Menus.Character.swapTo({selector: '.main-menu-items'});
       },
