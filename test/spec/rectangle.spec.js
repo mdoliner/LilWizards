2bdc6200e9242c8c9e82b9a61e73c70eb4444146
Justin <jaschultz25@gmail.com>
2015-10-11 15:54:28 -0600

    Adds new graphics.

--- test/spec/rectangle.spec.js
+++ test/spec/rectangle.spec.js
@@ -15,11 +15,13 @@
     };
 
     var vecString = function(coord) {
-      return '(' + coord.x + ', ' + coord.y ')'
+      coord = new LW.Coord(coord);
+      return '(' + coord.x + ', ' + coord.y + ')'
     };
 
-    var EPSILON = 1e-6;
+    var EPSILON = 1e-3;
     var checkCoords = function(coord, check) {
+      check = new LW.Coord(check);
       return Math.abs(coord.x - check.x) < EPSILON && Math.abs(coord.y - check.y) < EPSILON;
     };
 
@@ -28,7 +30,7 @@
       var rect;
       beforeEach(function() {
         rect = new LW.Rectangle({ x: 5, y: 5, width: 10, height: 10, angle: 0});
-      })
+      });
 
       it('should correctly return the defaults if no rotation', function() {
         var axis = rect.getAxis();
@@ -40,6 +42,43 @@
         }
       });
 
+      it('should correctly return the proper axis if rotation', function() {
+        rect.angle = 45;
+        var axis = rect.getAxis();
+        var checks = [new LW.Coord([0.707106, 0.707106]), new LW.Coord([-0.707106, 0.707106])];
+        for (var i = 0, n = axis.length; i < n; i++) {
+          var ax = axis[i];
+          var bool = checkCoords(ax, checks[0]) || checkCoords(ax, checks[1]);
+          expect(bool, 'axis' + vecString(ax) + ' to be close to ' + vecString(checks[0]) + ' or ' + vecString(checks[1])).to.equal(true);
+        }
+      });
+
+    });
+
+    describe('#corners', function() {
+
+      var rect;
+      beforeEach(function() {
+        rect = new LW.Rectangle({ x: 5, y: 5, width: 10, height: 10, angle: 0});
+      });
+
+      it('should correctly give the coordinates of a non-rotated rect', function() {
+        var corners = rect.getCorners();
+        expect(checkCoords(corners.TL, [5, 5]), 'corner' + vecString(corners.TL) + ' to be close to ' + vecString([5, 5])).to.equal(true);
+        expect(checkCoords(corners.TR, [15, 5]), 'corner' + vecString(corners.TR) + ' to be close to ' + vecString([15, 5])).to.equal(true);
+        expect(checkCoords(corners.BR, [15, 15]), 'corner' + vecString(corners.BR) + ' to be close to ' + vecString([15, 15])).to.equal(true);
+        expect(checkCoords(corners.BL, [5, 15]), 'corner' + vecString(corners.BL) + ' to be close to ' + vecString([5, 15])).to.equal(true);
+      });
+
+      it('should correctly give the coordinates of a rotated rect', function() {
+        rect.angle = 90;
+        var corners = rect.getCorners();
+        expect(checkCoords(corners.TL, [15, 5]), 'corner' + vecString(corners.TL) + ' to be close to ' + vecString([15, 5])).to.equal(true);
+        expect(checkCoords(corners.TR, [15, 15]), 'corner' + vecString(corners.TR) + ' to be close to ' + vecString([15, 15])).to.equal(true);
+        expect(checkCoords(corners.BR, [5, 15]), 'corner' + vecString(corners.BR) + ' to be close to ' + vecString([5, 15])).to.equal(true);
+        expect(checkCoords(corners.BL, [5, 5]), 'corner' + vecString(corners.BL) + ' to be close to ' + vecString([5, 5])).to.equal(true);
+      });
+
     });
 
     describe('#collision', function() {
@@ -47,7 +86,7 @@
       var rect;
       beforeEach(function() {
         rect = new LW.Rectangle({ x: 5, y: 5, width: 10, height: 10, angle: 0});
-      })
+      });
 
       it('should collide with something the matches its properties', function() {
         var rect2 = new LW.Rectangle(rect);
