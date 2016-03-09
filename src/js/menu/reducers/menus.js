fa10a0590689e4afa6e9c9c3e5f30984e0398671
Justin <jaschultz25@gmail.com>
2016-03-08 20:35:50 -0700

    Remerges menus and menu, so that recursion is possible.

--- src/js/menu/reducers/menus.js
+++ src/js/menu/reducers/menus.js
@@ -3,11 +3,16 @@
  */
 import _ from 'lodash';
 import getLayer from '../get_layer';
-import menuReducer, { createMenu } from './menu';
-import { List } from 'Immutable';
+import { List, fromJS } from 'Immutable';
 
 const initialState = List([createMenu('top')]);
 
+/**
+ * Reduces multiple menus.
+ * @param state
+ * @param action
+ * @returns {*}
+ */
 export default function menusReducer(state = initialState, action) {
   // Update the current menu
   state = state.update(-1, current => menuReducer(current, action));
@@ -31,3 +36,58 @@ export default function menusReducer(state = initialState, action) {
     }
   }
 };
+
+/**
+ * Reduces a single menu.
+ * @param state
+ * @param action
+ * @returns {*}
+ */
+function menuReducer(state, action) {
+  const layer = getLayer(state.get('layerName'));
+  const { columns, commands } = layer;
+  const numCommands = commands && commands.length;
+
+  // Resolve the parameters
+  const parameter = action.parameter || {};
+  const { direction, player } = parameter;
+
+  // Detect action types
+  switch (action.type) {
+    case 'SELECT': {
+      return state.update('index', idx => (idx + direction + numCommands) % numCommands);
+    }
+
+    case 'SELECT_COLUMN': {
+      const mathFn = Math[direction < 0 ? 'ceil' : 'floor'];
+      const columnAdjust = mathFn(direction *  numCommands / (columns || 1));
+      return state.update('index', idx => (idx + columnAdjust + numCommands) % numCommands);
+    }
+
+    case 'ADD_CHILD': {
+      if (state.get('subMenus').size > 3) {
+        console.log('too many children');
+        return state;
+      }
+
+      return state.setIn(['subMenus', player], [createMenu(layer.subMenuEntry)]);
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+/**
+ * Helper function.
+ * @param location
+ * @returns {*}
+ */
+function createMenu(location) {
+  return fromJS({
+    layerName: location,
+    index: 0,
+    subMenus: {},
+  });
+}
